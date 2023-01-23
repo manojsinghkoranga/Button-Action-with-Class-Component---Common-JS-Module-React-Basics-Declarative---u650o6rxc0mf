585089c5e1644cde3890559796433865dc292dc6
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-23 08:27:16 +0000

    working fine

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
+class App extends React.Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			doRenderPTag: false,
+		}
+		this.handleClick = this.handleClick.bind(this);
 	};
+
+	handleClick () {
+		this.setState({doRenderPTag: true});
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={this.handleClick}>Render P tag</button>
+				{this.state.doRenderPTag && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     		</div>
     	);
     }
 }
-
 
 export default App;
 
